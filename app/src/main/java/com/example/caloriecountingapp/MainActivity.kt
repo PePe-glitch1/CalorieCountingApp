e9114ec87ceec41e01459ed1acfff10d4178925c
Danil <danilbarabash@icloud.com>
2025-09-02 15:57:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/caloriecountingapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.caloriecountingapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.caloriecountingapp.ui.theme.CalorieCountingAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            getText() 
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun getText() {
+    Text(text = "Hello World!")
+}
