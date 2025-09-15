24714c6bac84a5ab698a2c39b0a1a035d549ad76
Danil <danilbarabash@icloud.com>
2025-09-15 15:31:23 +0300

    add top bar

--- app/src/main/java/com/example/caloriecountingapp/MainActivity.kt
+++ app/src/main/java/com/example/caloriecountingapp/MainActivity.kt
@@ -4,18 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -26,14 +41,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Column (
-                modifier = Modifier.
-                background(Color.Gray).fillMaxSize(),
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxSize(),
             ) {
-                topInfoContainer()
-                progresAllContainer()
-                dividedCalorieContainer()
-                infoBlockContainer()
-                bottomInfoContainer()
+                TopBar(name = "Top Bar")
 
             }
 
@@ -41,77 +53,44 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun topInfoContainer() {
-    Column (
+private fun TopBar (name: String) {
+    Card(
         modifier = Modifier
-            .background(Color.Cyan)
-            .height(30.dp)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .padding(top = 55.dp, start = 14.dp, end = 14.dp)
+            .height(62.dp)
     ) {
-        Text(text = "Hello Ass")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun progresAllContainer() {
-    Column (
-        modifier = Modifier
-            .background(Color.Green)
-            .fillMaxHeight(0.2f)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(text = "Hello Dildo")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun dividedCalorieContainer() {
-    Column (
-        modifier = Modifier
-            .background(Color.Yellow)
-            .fillMaxHeight(0.2f)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(text = "Hello Dildo")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun infoBlockContainer() {
-    Column (
-        modifier = Modifier
-            .background(Color.Magenta)
-            .fillMaxHeight(0.2f)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(text = "Hello Dildo")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun bottomInfoContainer() {
-    Column (
-        modifier = Modifier
-            .background(Color.Blue)
-            .fillMaxHeight(0.2f)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(text = "Hello Dildo")
+        Box(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxSize(),
+        ) {
+            Row (verticalAlignment = Alignment.CenterVertically,) {
+                Image(
+                    painter = painterResource(id = R.drawable.im_profil_imeg),
+                    contentDescription = "menuIcon",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(62.dp).clip(CircleShape)
+                    )
+                Column (modifier = Modifier.padding(start = 14.dp)) {
+                    Text(text = "Welcome")
+                    Text(text = name)
+                }
+                Row (
+                    verticalAlignment = Alignment.End,
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_fined),
+                        contentDescription = "menuIcon",
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_notification),
+                        contentDescription = "menuIcon",
+                        modifier = Modifier.size(20.dp)
+                    )
+                }
+            }
+        }
     }
 }
