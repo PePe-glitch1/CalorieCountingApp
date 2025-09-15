6ce100f5cf44c259d01ddb738ed886c8b631498f
Danil <danilbarabash@icloud.com>
2025-09-15 15:52:54 +0300

    Add finder and notification to top bar

--- app/src/main/java/com/example/caloriecountingapp/MainActivity.kt
+++ app/src/main/java/com/example/caloriecountingapp/MainActivity.kt
@@ -6,21 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,9 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -53,6 +47,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 private fun TopBar (name: String) {
     Card(
@@ -65,28 +60,32 @@ private fun TopBar (name: String) {
                 .background(Color.White)
                 .fillMaxSize(),
         ) {
-            Row (verticalAlignment = Alignment.CenterVertically,) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Image(
                     painter = painterResource(id = R.drawable.im_profil_imeg),
                     contentDescription = "menuIcon",
                     contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(62.dp).clip(CircleShape)
+                    modifier = Modifier
+                        .size(62.dp)
+                        .clip(CircleShape)
                     )
                 Column (modifier = Modifier.padding(start = 14.dp)) {
                     Text(text = "Welcome")
                     Text(text = name)
                 }
                 Row (
-                    verticalAlignment = Alignment.End,
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(24.dp, Alignment.End)
                 ) {
                     Image(
                         painter = painterResource(id = R.drawable.ic_fined),
-                        contentDescription = "menuIcon",
+                        contentDescription = "finder",
                         modifier = Modifier.size(20.dp)
                     )
                     Image(
                         painter = painterResource(id = R.drawable.ic_notification),
-                        contentDescription = "menuIcon",
+                        contentDescription = "notification",
                         modifier = Modifier.size(20.dp)
                     )
                 }
