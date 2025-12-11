7602e02150f4fde17326caea4badd6792a34bb05
Danil <danilbarabash@icloud.com>
2025-12-11 14:33:30 +0200

    Add Prewiew to see TopBar in real taime

--- app/src/main/java/com/example/caloriecountingapp/screenElements/TopBar.kt
+++ app/src/main/java/com/example/caloriecountingapp/screenElements/TopBar.kt
@@ -14,10 +14,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.caloriecountingapp.R
 
-
+@Preview
 @Composable
 fun TopBar(name: String = "User Name") {
     Card(
