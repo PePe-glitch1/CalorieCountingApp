9ec6b0976a2a5364ca07ba5cd515fa30e82ebf41
Danil <danilbarabash@icloud.com>
2025-12-14 09:32:42 +0200

    Add font golder. Add font kurale_regular, add in to text in TopBar

--- app/src/main/java/com/example/caloriecountingapp/screenElements/TopBar.kt
+++ app/src/main/java/com/example/caloriecountingapp/screenElements/TopBar.kt
@@ -18,7 +18,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.caloriecountingapp.R
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val Kurale_reg = FontFamily(
+    Font(R.font.kurale_regular, FontWeight.Normal)
+)
 
 @Preview
 @Composable
@@ -48,9 +56,20 @@ fun TopBar(name: String = "User Name") {
                         .clickable { Log.d("TopBar", "Profile image clicked") }
                 )
                 Spacer(Modifier.width(12.dp))
-                Column {
-                    Text(text = "Welcome")
-                    Text(text = name)
+                Column{
+                    Text(
+                        text = "Welcome",
+                        fontSize = 16.sp,
+                        color = Color.Gray,
+                        fontFamily = Kurale_reg,
+
+                    )
+
+                    Text(
+                        text = name,
+                        fontSize = 19.sp,
+                        fontFamily = Kurale_reg,
+                    )
                 }
             }
             Row(verticalAlignment = Alignment.CenterVertically) {
