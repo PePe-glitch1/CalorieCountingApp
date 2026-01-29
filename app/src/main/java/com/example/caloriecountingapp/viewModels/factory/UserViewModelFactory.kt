23ad9a660600553d8077f4aa3a19bb722ec63a74
Danil <danilbarabash@icloud.com>
2026-01-29 11:58:48 +0200

    Add UserViewModelFactory. Need to add for all Repositorys

---
+++ app/src/main/java/com/example/caloriecountingapp/viewModels/factory/UserViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.example.caloriecountingapp.viewModels.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.caloriecountingapp.viewModels.UserViewModel
+import com.example.domain.repository.UserRepository
+
+class UserViewModelFactory (
+    private val repository: UserRepository
+): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return UserViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
